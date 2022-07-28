801af85678b65649ff04660d0f5213d0d863bfd2
luharine <kattarahul2001@gmail.com>
2022-07-29 01:44:19 +0530

    fixed Currentissue.jsx

--- src/components/CurrentIssue/CurrIssue.jsx
+++ src/components/CurrentIssue/CurrIssue.jsx
@@ -1,13 +1,26 @@
 import React, { useState } from 'react'
 import './CurrentIssue.css'
+import { arr } from '../AddIssue/AddIssue';
 
 
 function CurrIssue(props) {
     const [addIssue,setAddIssue]=useState(false);
     const [showIssue,setShowIssue] = useState(true)
-    console.log("asdad") ;
-    console.log(props.obj);
     
+    
+    function deleteobj()
+    {    setShowIssue(false);
+       // console.log(arr);
+        console.log(props.obj)
+        console.log(arr);
+        arr.splice(arr.findIndex(a => a.AssignedTo === props.obj.AssignedTo) , 1)
+
+        //arr.splice(arr.findIndex(props.obj),1);
+        // console.log("deleted" + props)
+        console.log(arr)
+        
+       
+    }
    
 return ( showIssue &&
     
@@ -19,7 +32,7 @@ return ( showIssue &&
     <div className='current-issues'>
         <div className='indv-issue'>
 
-            <p onClick={()=>{setShowIssue(false)}} className='close-issue'>Close Issue</p>
+            <p onClick={()=>{deleteobj() }   } className='close-issue'>Close Issue</p>
         <p >Assigned To <span key={props.obj.AssignedTo} > {props.obj.AssignedTo} </span></p>
         <p>Priority <span key={props.obj.priority}>{props.obj.priority} </span></p>
         <p className='description-text'>Description<span key={props.obj.description}> {props.obj.description}</span></p>
